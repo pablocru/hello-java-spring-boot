0a7e861109e244545464e880f2a5e179cc8dd40d
pablocru <pablocru@outlook.com>
2024-09-06 14:20:00 +0200

    feat(custom-properties): Define custom data and change embedded server port and context path by updating the `application.properties` file

---
+++ workshops/01-spring-boot-overview/03-custom-properties/src/main/java/com/pablocru/custom_properties/CustomPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.pablocru.custom_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomPropertiesApplication.class, args);
+    }
+
+}
---
+++ workshops/01-spring-boot-overview/03-custom-properties/src/test/java/com/pablocru/custom_properties/CustomPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablocru.custom_properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomPropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
